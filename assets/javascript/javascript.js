b05efb14230b1a792adfc7333b4b79b2dd5ab9f3
kirti18patel <kirti18patel@yahoo.com>
2021-05-04 15:13:25 -0400

    form javascript added

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,10 +1,11 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
     
-var createTaskHandler = function() {
-    var taskItemEl = document.createElement("li");
-    taskItemEl.textContent="This is a new task.";
-    taskItemEl.className = "task-item";
-    tasksToDoEl.appendChild(taskItemEl);
+var createTaskHandler = function(event) {
+    event.preventDefault();
+    var listItemEl = document.createElement("li");
+    listItemEl.textContent="This is a new task.";
+    listItemEl.className = "task-item";
+    tasksToDoEl.appendChild(listItemEl);
 };
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
