8b45e4785811de798a7098706c42022395964815
kirti18patel <kirti18patel@yahoo.com>
2021-05-06 23:47:11 -0400

    edit functionality partially added

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -98,11 +98,26 @@ var deleteTask = function(taskId) {
     var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
     taskSelected.remove();
   };
+
+var editTask = function(taskId) {
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+    var taskName = taskSelected.querySelector("h3.task-name").textContent;
+    var taskType = taskSelected.querySelector("span.task-type").textContent;
+    document.querySelector("input[name='task-name']").value=taskName;
+    document.querySelector("select[name='task-type']").value=taskType;
+    document.querySelector("#save-task").textContent="Save Task";
+
+    formEl.setAttribute("data-task-id", taskId);
+};
 var taskButtonHandler = function(event) {
-    console.log(event.target);
-    if(event.target.matches(".delete-btn")){
+    var targetEl = event.target;
+    if(event.target.matches(".edit-btn")){
+        var taskId= event.target.getAttribute("data-task-id");
+        editTask(taskId);
+    }
+    else if(event.target.matches(".delete-btn")){
         var taskId= event.target.getAttribute("data-task-id");
         deleteTask(taskId);
     }
-  };
+};
 pageContentEl.addEventListener("click", taskButtonHandler);
